d5abb8f61a2ad870c63954c132b883164bf2b2ea
Elizabeth Griffith <elizabethgriffith97@gmail.com>
2022-08-22 11:04:34 -0400

    Add function to sort items into selected category

---
+++ app.js
@@ -0,0 +1,46 @@
+// Define UI Variables
+const form = document.querySelector('#itemForm')
+const itemInput = document.querySelector('#item')
+const categoryInput = document.querySelector('#category')
+const groceryList = document.querySelector('.groceryCollection')
+const bigBoxList = document.querySelector('.bigBoxCollection')
+const homeImprovementList = document.querySelector('.homeImprovementCollection')
+
+
+
+// Load all event listeners
+loadEventListeners()
+
+function loadEventListeners(){
+  // Add item
+  form.addEventListener('submit', addForm)
+}
+
+function addForm(e){
+  // alert if empty fields
+  if (itemInput.value === ''){
+    alert('You must add an item name to add it to the list!')
+  } else if (categoryInput.value === 'select'){
+    alert('You must select a category to add it to the list!')
+  }
+  
+  // create and append li with anchor and icon elements
+  const li = document.createElement('li')
+  li.appendChild(document.createTextNode(itemInput.value))
+  const link = document.createElement('a')
+  link.className = 'delete-item'
+  link.innerHTML = '<i class = "fa-solid fa-xmark"></i>'
+  li.appendChild(link)
+  
+  // append li to correct category
+  if (categoryInput.value === 'grocery'){
+    groceryList.appendChild(li)
+  } else if (categoryInput.value === 'boxStore'){
+    bigBoxList.appendChild(li)
+  } else {
+    homeImprovementList.appendChild(li)
+  }
+
+
+  e.preventDefault()
+}
