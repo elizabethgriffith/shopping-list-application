9256282e3ab0286a50df0790450499dda2de7a93
Elizabeth Griffith <elizabethgriffith97@gmail.com>
2022-08-22 12:02:06 -0400

    Store item and category in local storage as an object

--- app.js
+++ app.js
@@ -12,9 +12,40 @@ const homeImprovementList = document.querySelector('.homeImprovementCollection')
 loadEventListeners()
 
 function loadEventListeners(){
+  // DOM load
+  //document.addEventListener('DOMContentLoaded', getItems)
   // Add item
   form.addEventListener('submit', addForm)
 }
+
+// function getItems(){
+//   let items 
+//   if(localStorage.getItem('items') === null){
+//     items = []
+//   } else {
+//     items = JSON.parse(localStorage.getItem('items'))
+//   }
+
+//   items.forEach(function(item){
+//     // create and append li with anchor and icon elements
+//     const li = document.createElement('li')
+//     li.appendChild(document.createTextNode(item))
+//     const link = document.createElement('a')
+//     link.className = 'delete-item'
+//     link.innerHTML = '<i class = "fa-solid fa-xmark"></i>'
+//     li.appendChild(link)
+  
+//     // append li to correct category
+//     if (categoryInput.value === 'grocery'){
+//      groceryList.appendChild(li)
+//     } else if (categoryInput.value === 'boxStore'){
+//       bigBoxList.appendChild(li)
+//     } else {
+//       homeImprovementList.appendChild(li)
+//     }
+
+//   })
+// }
 
 function addForm(e){
   // alert if empty fields
@@ -41,6 +72,47 @@ function addForm(e){
     homeImprovementList.appendChild(li)
   }
 
+  // store items in local storage
+  storeItemInLocalStorage(itemInput.value, categoryInput.value)
+
+
+  // clear input
+  // itemInput.value = ''
+  // categoryInput.value = 'select'
 
   e.preventDefault()
+}
+
+function storeItemInLocalStorage(item, category){
+  let items
+
+  let itemObj = {
+    itemName: item,
+    categoryName: category
+  } 
+
+  
+  if(localStorage.getItem('items') === null){
+    items = []
+  } else {
+    items = JSON.parse(localStorage.getItem('items'))
+  }
+
+  items.push(itemObj)
+
+  localStorage.setItem('items', JSON.stringify(items))
+}
+
+function storeCategoryInLocalStorage(category){
+  let cate
+  if(localStorage.getItem('items') === null){
+    items = []
+  } else {
+    items = JSON.parse(localStorage.getItem('items'))
+  }
+
+  items.push(item)
+
+  localStorage.setItem('items', JSON.stringify(items))
+
 }
